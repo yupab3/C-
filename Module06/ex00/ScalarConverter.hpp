bfc7381d62549da1ff2927236e69a7e7a3b58eb3
dongyeuk <yupab3@naver.com>
2024-05-23 16:45:01 +0900

    finish module06 ex00

--- Module06/ex00/ScalarConverter.hpp
+++ Module06/ex00/ScalarConverter.hpp
@@ -6,6 +6,11 @@
 class ScalarConverter{
 public:
 	static void convert(const std::string& _trgt);
+private:
+	ScalarConverter(void);
+	ScalarConverter(const ScalarConverter& _copy);
+	ScalarConverter& operator=(const ScalarConverter& _copy);
+	~ScalarConverter(void);
 };
 
 #endif
